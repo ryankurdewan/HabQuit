48e09f012282bc4284926859a572c255aec25fa5
weeksj13 <weeksj13@students.ecu.edu>
2017-01-19 13:05:16 -0500

    Bottom navigation bar - needs work

--- app/src/main/java/com/aquamorph/habquit/activities/MainActivity.java
+++ app/src/main/java/com/aquamorph/habquit/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.aquamorph.habquit.activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,11 +17,47 @@ public class MainActivity extends AppCompatActivity {
 
 	private static int cigaretteCount = 0;
 	private String beginText = "";
+    private TextView textFavorites;
+    private TextView textSchedules;
+    private TextView textMusic;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        textFavorites = (TextView) findViewById(R.id.text_favorites);
+        textSchedules = (TextView) findViewById(R.id.text_schedules);
+        textMusic = (TextView) findViewById(R.id.text_music);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_favorites:
+                                textFavorites.setVisibility(View.VISIBLE);
+                                textSchedules.setVisibility(View.GONE);
+                                textMusic.setVisibility(View.GONE);
+                                break;
+                            case R.id.action_schedules:
+                                textFavorites.setVisibility(View.GONE);
+                                textSchedules.setVisibility(View.VISIBLE);
+                                textMusic.setVisibility(View.GONE);
+                                break;
+                            case R.id.action_music:
+                                textFavorites.setVisibility(View.GONE);
+                                textSchedules.setVisibility(View.GONE);
+                                textMusic.setVisibility(View.VISIBLE);
+                                break;
+                        }
+                        return true;
+                    }
+                }
+        );
 
 		CardView cigarette = (CardView) findViewById(R.id.habit1);
 		final TextView smokingCountText = (TextView) findViewById(R.id.textView2);
