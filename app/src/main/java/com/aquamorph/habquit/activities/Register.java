b7eb0eb497d8ac3d4c1dde333bf5ed114cce0af7
Ryan Summerlin <summerlinryan@gmail.com>
2017-01-20 13:54:54 -0500

    Gave a more specific id to the cancel button. Linked the cancel button on the Register page back to the Login page.

--- app/src/main/java/com/aquamorph/habquit/activities/Register.java
+++ app/src/main/java/com/aquamorph/habquit/activities/Register.java
@@ -1,7 +1,10 @@
 package com.aquamorph.habquit.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.aquamorph.habquit.R;
 
@@ -11,5 +14,21 @@ public class Register extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register_page);
+
+        Button cancelButton = (Button) findViewById(R.id.CancelButton);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogin();
+            }
+        });
 	}
+
+    /**
+     * openLogin() starts login activity
+     */
+    public void openLogin() {
+        startActivity(new Intent(this, Login.class));
+    }
 }
