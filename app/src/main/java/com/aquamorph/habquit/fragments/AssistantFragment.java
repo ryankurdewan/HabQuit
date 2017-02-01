ae0bb5c9e6b66fabc761c763c0c61093cce96431
Christian Colglazier <aquamorphcomputing@gmail.com>
2017-02-01 14:12:25 -0500

    Added mood changing to the assistant

--- app/src/main/java/com/aquamorph/habquit/fragments/AssistantFragment.java
+++ app/src/main/java/com/aquamorph/habquit/fragments/AssistantFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.aquamorph.habquit.R;
 
@@ -17,9 +19,76 @@ import com.aquamorph.habquit.R;
 
 public class AssistantFragment extends Fragment {
 
+	TextView assistantMessageText;
+	ImageView assistant;
+	private int mood = 50;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_assistant, container, false);
+		View view = inflater.inflate(R.layout.fragment_assistant, container, false);
+		assistantMessageText = (TextView) view.findViewById(R.id.assistant_message);
+		assistant = (ImageView) view.findViewById(R.id.assistant);
+		assistant.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				changeMood(5);
+				if (getMood() < 60 && getMood() > 45)
+					sendMessage("I see you have a clear wish to die alone by age 40");
+				else
+					sendMessage("My mood is " + getMood());
+			}
+		});
+		assistant.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View view) {
+				changeMood(-5);
+				sendMessage("My mood is " + getMood());
+				return true;
+			}
+		});
+		checkMood();
+		return view;
+	}
+
+	/**
+	 * Allow to 
+	 * @param text
+	 */
+	public void sendMessage(String text) {
+		if (assistantMessageText != null) {
+			assistantMessageText.setText(text);
+		}
+	}
+
+	public int getMood() {
+		return mood;
+	}
+
+	/**
+	 * Changes the mood value of the assistant.
+	 *
+	 * @param changeAmount sets how much you want to adjust the mood.
+	 */
+	public void changeMood(int changeAmount) {
+		mood = mood + changeAmount;
+		if (mood > 100) mood = 100;
+		else if (mood < 0) mood = 0;
+		checkMood();
+	}
+
+	/**
+	 * Checks the mood of the AI and changes the design based on its mood.
+	 */
+	private void checkMood() {
+		if (assistant == null) {
+
+		} else if (getMood() < 40) {
+			assistant.setImageResource(R.drawable.ai_mad);
+		} else if (getMood() < 60) {
+			assistant.setImageResource(R.drawable.ai_neutral);
+		} else {
+			assistant.setImageResource(R.drawable.ai_happy);
+		}
 	}
 }
