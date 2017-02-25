c495884e70065c77026cee007e0d7f91fd8c3d75
Ryan Summerlin <summerlinr14@students.ecu.edu>
2017-02-24 23:41:01 -0500

    created the basic framework for the add new habit page

--- app/src/main/java/com/aquamorph/habquit/activities/AddHabitActivity.java
+++ app/src/main/java/com/aquamorph/habquit/activities/AddHabitActivity.java
@@ -3,6 +3,9 @@ package com.aquamorph.habquit.activities;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.aquamorph.habquit.R;
 
@@ -11,9 +14,25 @@ import com.aquamorph.habquit.R;
  */
 
 public class AddHabitActivity extends AppCompatActivity {
+    EditText habitName;
+    EditText habitPricePerUnit;
+    Button addHabitButton;
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.add_habit);
+
+        habitName = (EditText) findViewById(R.id.add_habit_name);
+        habitPricePerUnit = (EditText) findViewById(R.id.add_habit_price);
+        addHabitButton = (Button) findViewById(R.id.add_habit_button);
+
+        addHabitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.printf("Added new habit with name: %s and price: %s\n",
+                        habitName.getText().toString(), habitPricePerUnit.getText().toString());
+            }
+        });
     }
 }
