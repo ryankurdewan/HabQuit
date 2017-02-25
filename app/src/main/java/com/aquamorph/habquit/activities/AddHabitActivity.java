875973d999e8a3a64e168683a3784341d796f993
Ryan Summerlin <summerlinr14@students.ecu.edu>
2017-02-25 18:28:47 -0500

    made progress on storing custom habit information locally with sharedpreferences. still some work to be done

--- app/src/main/java/com/aquamorph/habquit/activities/AddHabitActivity.java
+++ app/src/main/java/com/aquamorph/habquit/activities/AddHabitActivity.java
@@ -1,7 +1,8 @@
 package com.aquamorph.habquit.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,30 +10,50 @@ import android.widget.EditText;
 
 import com.aquamorph.habquit.R;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by ryansummerlin on 2/16/17.
  */
 
 public class AddHabitActivity extends AppCompatActivity {
-    EditText habitName;
-    EditText habitPricePerUnit;
-    Button addHabitButton;
+    EditText customHabitName;
+    EditText customHabitPrice;
+    Button customHabitButton;
+
+    SharedPreferences preferences;
+    final String PREFERENCES_FILE_NAME = "CustomHabitPreferences";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_habit);
 
-        habitName = (EditText) findViewById(R.id.add_habit_name);
-        habitPricePerUnit = (EditText) findViewById(R.id.add_habit_price);
-        addHabitButton = (Button) findViewById(R.id.add_habit_button);
+        customHabitName = (EditText) findViewById(R.id.add_habit_name);
+        customHabitPrice = (EditText) findViewById(R.id.add_habit_price);
+        customHabitButton = (Button) findViewById(R.id.add_habit_button);
+        preferences = getSharedPreferences(PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
 
-        addHabitButton.setOnClickListener(new View.OnClickListener() {
+        customHabitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.printf("Added new habit with name: %s and price: %s\n",
-                        habitName.getText().toString(), habitPricePerUnit.getText().toString());
+                addHabit(preferences,
+                         customHabitName.getText().toString(),
+                         customHabitPrice.getText().toString());
+
             }
         });
+    }
+
+    private void addHabit(SharedPreferences preferences, String name, String price) {
+//        Set<String> customHabits = preferences.getStringSet("custom", null);
+//
+//        if (customHabits != null) {
+//            customHabits.add(name + ":" + price);
+//        } else {
+//            // TODO: determine how to map 'custom' string to a name->price collection.
+//            customHabits = new HashSet<>();
+//        }
     }
 }
