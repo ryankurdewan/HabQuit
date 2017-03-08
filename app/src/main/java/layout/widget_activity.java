239774f4bc4f7ff4fc42f5981480bec59ce4cf6f
weeksj13 <weeksj13@students.ecu.edu>
2017-03-08 16:11:40 -0500

    Widget updates text view on button click.  Need to link habit to configuration of widget.

--- app/src/main/java/layout/widget_activity.java
+++ app/src/main/java/layout/widget_activity.java
@@ -4,6 +4,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 import android.content.Intent;
 import android.widget.Toast;
@@ -22,45 +23,77 @@ import static android.R.style.Widget;
 //Widget activity added by JCW
 public class widget_activity extends AppWidgetProvider {
 
-    private static final String SYNC_CLICKED    = "automaticWidgetSyncButtonClick";
+    private static final String SYNC_CLICKED    = "AppWidgetManager.ACTION_APPWIDGET_UPDATE";
     private static int incCount = 0;
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = widget_activityConfigureActivity.loadTitlePref(context, appWidgetId);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_activity);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        System.out.println("updateAppWidget(): widgetId = " + String.valueOf(appWidgetId) + "... Count : " +  String.valueOf(incCount));
+
+        Toast.makeText(context, "updateAppWidget(): widgetId = " + String.valueOf(appWidgetId) + "... Count : " +  String.valueOf(incCount), Toast.LENGTH_LONG).show();
+
+        // set up remote view and set text views
+        RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_activity);
+        updateViews.setTextViewText(R.id.appWidgetText, String.valueOf(incCount));
+        updateViews.setTextViewText(R.id.appwidget_text, "app widget id: [" + String.valueOf(appWidgetId) + "]");
+
+        ComponentName thisWidget = new ComponentName(context, widget_activity.class);
+
+        Intent intent = new Intent(context, widget_activity.class);
+        intent.setAction(SYNC_CLICKED);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetId);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        updateViews.setOnClickPendingIntent(R.id.appWidgetButton, pendingIntent);
 
         // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        appWidgetManager.updateAppWidget(thisWidget, updateViews);
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (SYNC_CLICKED.equals(intent.getAction())) {
+
+            incCount++;
+
+            Bundle extras = intent.getExtras();
+
+            if(extras!=null) {
+
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+                // ComponentName thisAppWidget = new ComponentName(context, AppWidgetProvider.class);
+
+                RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_activity);
+                CharSequence widgetCount = String.valueOf(incCount);
+                remoteViews.setTextViewText(R.id.appWidgetText, widgetCount);
+
+                int widgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+
+                Toast.makeText(context, "onReceive(): widgetId = " + String.valueOf(widgetId) + "... Count : " +  String.valueOf(incCount), Toast.LENGTH_LONG).show();
+
+                System.out.println("onReceive(): widgetId = " + String.valueOf(widgetId) + "... Count : " +  String.valueOf(incCount));
+
+                updateAppWidget(context, appWidgetManager, widgetId);
+
+            }
+        }
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
 
-        System.out.println("on-update widget");
-
-        RemoteViews remoteViews;
-        ComponentName watchWidget;
-
-        for (int appWidgetId : appWidgetIds) {
-            PendingIntent pendingIntent = getPendingSelfIntent(context, SYNC_CLICKED);
-            remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_activity);
-            watchWidget = new ComponentName(context, widget_activity.class);
-            remoteViews.setOnClickPendingIntent(R.id.widget_inc_button, pendingIntent);
-            appWidgetManager.updateAppWidget(watchWidget, remoteViews);
-        }
-
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         // When the user deletes the widget, delete the preference associated with it.
         for (int appWidgetId : appWidgetIds) {
+
             widget_activityConfigureActivity.deleteTitlePref(context, appWidgetId);
         }
     }
@@ -68,47 +101,18 @@ public class widget_activity extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+
+        Toast.makeText(context, "onEnabled()", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+
+
+        Toast.makeText(context, "onDisabled()", Toast.LENGTH_LONG).show();
     }
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        // TODO Auto-generated method stub
 
-        if(intent != null) {
-            if (SYNC_CLICKED.equals(intent.getAction())) {
-
-                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-
-                RemoteViews remoteViews;
-                ComponentName watchWidget;
-
-                incCount++;
-                String incCountStr = Float.toString(incCount);
-
-                remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_activity);
-                watchWidget = new ComponentName(context, widget_activity.class);
-
-                remoteViews.setTextViewText(R.id.widget_inc_button, incCountStr);
-
-                appWidgetManager.updateAppWidget(watchWidget, remoteViews);
-
-            }
-        }
-
-        //super.onReceive(context, intent);
-
-    }
-
-    protected PendingIntent getPendingSelfIntent(Context context, String action) {
-
-        Intent intent = new Intent(context, getClass());
-        intent.setAction(action);
-        return PendingIntent.getBroadcast(context, 0, intent, 0);
-    }
 }
 
