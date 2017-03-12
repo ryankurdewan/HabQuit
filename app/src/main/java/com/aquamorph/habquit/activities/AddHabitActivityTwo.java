fb7c218c0a7daf4405a236f9dcc969959dc1086d
Christian Colglazier <aquamorphcomputing@gmail.com>
2017-03-12 10:25:00 -0400

    Added back button to add habit page

--- app/src/main/java/com/aquamorph/habquit/activities/AddHabitActivityTwo.java
+++ app/src/main/java/com/aquamorph/habquit/activities/AddHabitActivityTwo.java
@@ -3,6 +3,7 @@ package com.aquamorph.habquit.activities;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,7 +41,20 @@ public class AddHabitActivityTwo extends AppCompatActivity {
 
 		startUpHabitID = getIntent().getExtras().getInt("HabitID");
 		isAddHabit = getIntent().getBooleanExtra("isAddHabit", true);
+
+		if (getSupportActionBar() != null) {
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+			if (isAddHabit)
+				getSupportActionBar().setTitle(getString(R.string.add_habit_button));
+			else
+				getSupportActionBar().setTitle("Manage Habit");
+		}
+
 		habitName = (EditText) findViewById(R.id.addHabitName);
+		habitPrice = (EditText) findViewById(R.id.add_habit_goal_per_day);
+		currPerDay = (EditText) findViewById(R.id.add_habit_goal_date);
+		goalPerDay = (EditText) findViewById(R.id.add_habit_price);
+		goalDate = (EditText) findViewById(R.id.add_habit_curr_per_day);
 
 		if (startUpHabitID != CUSTOM_ID) {
 			habitName.setFocusable(false);
@@ -73,7 +87,7 @@ public class AddHabitActivityTwo extends AppCompatActivity {
 		addHabitPriceWrapper.setHint(getString(R.string.add_habit_price_hint));
 		addHabitCurrPerDayWrapper.setHint(getString(R.string.add_habit_curr_per_day_hint));
 
-		if(isAddHabit)
+		if (isAddHabit)
 			addHabitButton.setText(getString(R.string.add_habit_button));
 		else
 			addHabitButton.setText(getString(R.string.delete_habit_button));
@@ -86,7 +100,33 @@ public class AddHabitActivityTwo extends AppCompatActivity {
 		});
 	}
 
-	private void addActivity() {
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				this.finish();
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
+	private void addActivity() {
+		if (habitName.getText().toString().trim().equals(""))
+			habitName.setError("Please enter a habit name");
+		else if (habitPrice.getText().toString().trim().equals(""))
+			habitPrice.setError("Please enter a habit price");
+		else if (currPerDay.getText().toString().trim().equals(""))
+			currPerDay.setError("Please enter a current number of habit usage");
+		else if (goalPerDay.getText().toString().trim().equals(""))
+			goalPerDay.setError("Please enter a desired number of habit usage");
+		else if (goalDate.getText().toString().trim().equals(""))
+			goalDate.setError("Please enter a goal date");
+		else {
+			if (isAddHabit) {
+
+			} else {
+
+			}
+			finish();
+		}
 	}
 }
